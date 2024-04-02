0ac6c104d0631b963cc9b264c15c96961599e780
david <gamboldarvin20@gmail.com>
2024-04-02 17:32:21 +0400

    categories page add

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,23 @@
+const { categoriesForm } = document.forms;
+
+function showToast(message, { type = 'error' } = {}) {
+  const toast = document.createElement('div');
+  toast.innerText = message;
+  toast.style.position = 'fixed';
+  toast.style.top = '20px';
+  toast.style.left = '50%';
+  toast.style.transform = 'translateX(-50%)';
+  toast.style.backgroundColor = type === 'error' ? '#ff000091' : '#00800075';
+  toast.style.color = 'white';
+  toast.style.padding = '10px 20px';
+  toast.style.borderRadius = '5px';
+  toast.style.zIndex = '1000';
+  document.body.appendChild(toast);
+  setTimeout(() => {
+    toast.remove();
+  }, 4000);
+}
+
 document.addEventListener('click', async (e) => {
   if (e.target.textContent === 'Сделать Админом') {
     try {
@@ -17,7 +37,7 @@ document.addEventListener('click', async (e) => {
       const li = document.getElementById(`${id}`);
       li.insertAdjacentHTML(
         'beforeend',
-        `<button type="button" class="btnUndoAdmin" data-id=${id} id="${id}-add">Убрать Админа</button>`,
+        `<button type="button" class="btnUndoAdmin" data-id=${id} id="${id}-undo">Убрать Админа</button>`,
       );
     } catch (error) {
       console.error(error);
@@ -35,7 +55,7 @@ document.addEventListener('click', async (e) => {
       if (res.status === 400) {
         alert('Что-то пошло не так');
       }
-      const btnId = document.getElementById(`${id}-add`);
+      const btnId = document.getElementById(`${id}-undo`);
       btnId.remove();
       const li = document.getElementById(`${id}`);
       li.insertAdjacentHTML(
@@ -45,5 +65,25 @@ document.addEventListener('click', async (e) => {
     } catch (error) {
       console.error(error);
     }
+  }
+});
+
+categoriesForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  try {
+    const data = new FormData(categoriesForm);
+    console.log(data);
+    const res = await fetch('/categories', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    });
+    if (res.status === 200) {
+      showToast('Категория добавлено', { type: 'success' });
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
