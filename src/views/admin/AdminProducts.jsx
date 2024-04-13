c6f0c2ff9d16b1ac12f35cf7a6c5b6dedc2411f7
Oleg Kosourov <olegk@mercury-labs.co>
2024-04-13 14:58:54 +0400

    Add titles in admin pages

--- src/views/admin/AdminProducts.jsx
+++ src/views/admin/AdminProducts.jsx
@@ -11,8 +11,17 @@ module.exports = function AdminProducts(props) {
         <div className="administrator">
           <div className="search-div">
             <form className="search-user" name="searchProduct" method="GET">
-              <img src="/svg/search.svg" alt="search" style={{ height: '25px' }} />
-              <input type="text" placeholder="Поиск Товара" name="search" value={search} />
+              <img
+                src="/svg/search.svg"
+                alt="search"
+                style={{ height: '25px' }}
+              />
+              <input
+                type="text"
+                placeholder="Поиск Товара"
+                name="search"
+                value={search}
+              />
               <button type="submit">Найти</button>
             </form>
             <a href="/product/new">
@@ -21,6 +30,7 @@ module.exports = function AdminProducts(props) {
               </button>
             </a>
           </div>
+          <h2>Товары</h2>
           <table className="all-products">
             <thead>
               <tr className="all-products-li-name">
@@ -37,17 +47,29 @@ module.exports = function AdminProducts(props) {
               {allProducts.map((product) => (
                 <tr className="all-products-li" id={product.id}>
                   <td className="product-img-td">
-                    <img className="product-img" src={product.imgPath} alt={product.name} />
+                    <img
+                      className="product-img"
+                      src={product.imgPath}
+                      alt={product.name}
+                    />
                   </td>
                   <td className="admin-productsName">{product.name}</td>
                   <td className="admin-products-name">{product.productCode}</td>
-                  <td className="admin-products-stock">{product.Manufacturer.name}</td>
+                  <td className="admin-products-stock">
+                    {product.Manufacturer.name}
+                  </td>
                   <td className="admin-products-name">{product.price}</td>
-                  <td className="admin-products-name">{product.quantityInStock}</td>
+                  <td className="admin-products-name">
+                    {product.quantityInStock}
+                  </td>
                   <td className="button-div">
                     <a href={`/product/${product.id}/edit`}>
                       <button type="button" className="edit-btn">
-                        <img className="edit" src="/svg/edit.svg" alt="Изменить" />
+                        <img
+                          className="edit"
+                          src="/svg/edit.svg"
+                          alt="Изменить"
+                        />
                       </button>
                     </a>
                     <button
@@ -70,37 +92,53 @@ module.exports = function AdminProducts(props) {
           {all.length > 10 && (
             <div className="back-next">
               <a
-                href={`/admin/products?${search ? `search=${search}&` : ''}page=${
-                  +page > 1 ? +page - 1 : +page
-                }`}
+                href={`/admin/products?${
+                  search ? `search=${search}&` : ''
+                }page=${+page > 1 ? +page - 1 : +page}`}
               >
                 <img src="/svg/left.svg" alt="back" />
               </a>
               {+page >= 3 && (
-                <a href={`/admin/products?${search ? `search=${search}&` : ''}page=${+page - 2}`}>
+                <a
+                  href={`/admin/products?${
+                    search ? `search=${search}&` : ''
+                  }page=${+page - 2}`}
+                >
                   {+page - 2}
                 </a>
               )}
               {+page >= 2 && (
-                <a href={`/admin/products?${search ? `search=${search}&` : ''}page=${+page - 1}`}>
+                <a
+                  href={`/admin/products?${
+                    search ? `search=${search}&` : ''
+                  }page=${+page - 1}`}
+                >
                   {+page - 1}
                 </a>
               )}
               <span>...</span>
               {+page === limit || (
-                <a href={`/admin/products?${search ? `search=${search}&` : ''}page=${+page + 1}`}>
+                <a
+                  href={`/admin/products?${
+                    search ? `search=${search}&` : ''
+                  }page=${+page + 1}`}
+                >
                   {+page + 1}
                 </a>
               )}
               {+page + 1 >= limit || (
-                <a href={`/admin/products?${search ? `search=${search}&` : ''}page=${+page + 2}`}>
+                <a
+                  href={`/admin/products?${
+                    search ? `search=${search}&` : ''
+                  }page=${+page + 2}`}
+                >
                   {+page + 2}
                 </a>
               )}
               <a
-                href={`/admin/products?${search ? `search=${search}&` : ''}page=${
-                  +page < limit ? +page + 1 : +page
-                }`}
+                href={`/admin/products?${
+                  search ? `search=${search}&` : ''
+                }page=${+page < limit ? +page + 1 : +page}`}
               >
                 <img src="/svg/right.svg" alt="next" />
               </a>
